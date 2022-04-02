f7fe4559af78a82cabceecc4a89761019d897e57
Pedro Muriel elduayen <pedmurie@c3r10s2.42madrid.com>
2022-04-02 12:29:42 +0200

    noze

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,50 +6,44 @@
 /*   By: pedmurie@student.42madrid.com <pedmurie    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 21:11:11 by pedmurie@st       #+#    #+#             */
-/*   Updated: 2022/03/28 22:09:34 by pedmurie@st      ###   ########.fr       */
+/*   Updated: 2022/04/02 12:25:34 by pedmurie@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 
-char	*ft_strnstr(const char *h, const char *needle, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t		a;
-	int			b;
-	int			c;
+	size_t	i;
+	size_t	j;
 
-	a = 0;
-	b = 0;
+	j = 0;
 	if (needle[0] == '\0')
-		return ((char *)h);
-	while (h[a] && a < len)
+		return ((char *)haystack);
+	while (j < len && haystack[j])
 	{
-		b = 0;
-		if (h[a] == needle[b])
+		i = 0;
+		while (needle[i] && haystack[j] && needle[i] == haystack[j] && j < len)
 		{
-			c = a;
-			while (needle[b] && needle[b] == h[c] && a < len)
-			{
-				c++;
-				b++;
-			}
-			if (needle[b - 1] == h[a])
-				return ((char *)&h[a]);
+			i++;
+			j++;
 		}
-		a++;
+		if (needle[i] == '\0')
+			return ((char *)&haystack[j - i]);
+		j++;
 	}
 	return (0);
 }
 /*
-int	main()
+int	main(void)
 {
 	char		*h;
 	char		*needle;
 	size_t		len;
 
-	h = "MZIRIaMZIRIBMZE123";
-	needle = "MZIRIBMZE123";
-	len = strlen(needle);
+	h = "lorem ipsum dolor sit amet";
+	needle = "dolor";
+	len = 20;
 	printf("%s\n", strnstr(h, needle, len));
 	printf("%s", ft_strnstr(h, needle, len));
 	return (0);
