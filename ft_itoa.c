4b29318b5d0192342a49360b7730d16c7adb1c6d
Pedro Muriel elduayen <pedmurie@c3r10s2.42madrid.com>
2022-04-02 11:47:33 +0200

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: pedmurie@student.42madrid.com <pedmurie    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 14:12:08 by pedmurie@st       #+#    #+#             */
-/*   Updated: 2022/03/30 17:11:06 by pedmurie@st      ###   ########.fr       */
+/*   Updated: 2022/04/02 11:45:03 by pedmurie@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,17 @@ int	ft_digit(unsigned int n)
 		a++;
 	}
 	return (a);
+}
+
+char	*ft_normi(int a, char *ptr, unsigned int n)
+{
+	while (a >= 0)
+	{
+		ptr[a] = n % 10 + 48;
+		n /= 10;
+		a--;
+	}
+	return (ptr);
 }
 
 char	*ft_mkstr(unsigned int n, int a, int w)
@@ -45,12 +56,7 @@ char	*ft_mkstr(unsigned int n, int a, int w)
 			return (NULL);
 		w *= -1;
 	}
-	while (a >= 0)
-	{
-		ptr[a] = n % 10 + 48;
-		n /= 10;
-		a--;
-	}
+	ptr = ft_normi(a, ptr, n);
 	if (cpy < 0)
 		ptr[0] = '-';
 	return (ptr);
@@ -68,10 +74,10 @@ char	*ft_itoa(int n)
 	a = ft_digit(j);
 	return (ft_mkstr(j, a, n));
 }
-
+/*
 int	main(void)
 {
 	printf("%s", ft_itoa(-214743648));
 	return (0);
 }
-
+*/
